69b3da091a9b3840291b74aa2aa72e0c1375c530
goandreus <andreshtml5@gmail.com>
2019-08-05 10:23:20 -0500

    navbar component and strcture

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <router-link :to="{name:'home'}">
-          <span>Vue</span>
-        </router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn @click="cerrarSesion">
-        <span class="mr-2">Cerrar Sesion</span>
-        <v-icon>fas fa-address-book</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+      <Navbar></Navbar>
     <v-content>
       <v-container>
         <router-view/>
       </v-container>
-      
     </v-content>
   </v-app>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import Navbar from '@/components/Navbar'
 export default {
+  components: {Navbar},
   name: 'App',
   data: () => ({
     //
   }),
-  methods:{
-    ...mapActions(['cerrarSesion'])
-  }
+  
 };
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <v-app-bar app>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <router-link :to="{name:'home'}">
+          <span>Vue</span>
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="cerrarSesion">
+        <span class="mr-2">Cerrar Sesion</span>
+        <v-icon>fas fa-address-book</v-icon>
+      </v-btn>
+    </v-app-bar>
+    </nav>
+</template>
+
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+    methods:{
+    ...mapActions(['cerrarSesion'])
+    }
+}
+</script>
