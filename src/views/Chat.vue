097bea6b2f80d99ea135390a45f470f0b2842298
goandreus <andreshtml5@gmail.com>
2019-08-18 16:44:45 -0500

    computed, methods

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -5,6 +5,22 @@
                 <v-card-text>
                     <h3>Bienvenido: {{usuario.nombre}}</h3>
                 </v-card-text>
+
+                <v-card-text>
+                    <div class="text-right mr-2">
+                        <v-chip>
+                            <v-avatar>
+                                <img :src="usuario.foto" alt="">
+                            </v-avatar> Mensaje de chat
+                        </v-chip>
+                        <p class="caption mr-2"> 06 febrero 2019 10AM</p>
+                    </div>
+                </v-card-text>
+                <v-card-text>
+                    <v-form @submit.prevent="enviarMensaje">
+                        <v-text-field v-model="mensaje" label="Escribe tu mensaje"></v-text-field>
+                    </v-form>
+                </v-card-text>
             </v-card>
         </v-flex>
     </v-layout>
@@ -15,13 +31,18 @@
     export default {
         data(){
         return{
-            
+            mensaje: ''
         }
     },
 
     computed: {
             ...mapState(['usuario'])
-        },
+        
+    },
+    methods: {
+        enviarMensaje(){
+            console.log('mensaje enviado', this.mensaje)
+        }
+    },
     }
-    
 </script>
